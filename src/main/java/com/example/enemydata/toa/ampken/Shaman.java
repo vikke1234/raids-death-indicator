c8cf14a9a6bb135ff7cc6ea6bccf2a6181ef45e3
Viktor Horsmanheimo <viktor.horsmanheimo@gmail.com>
2026-06-01 17:56:15 +0300

    toa: Update cursed and shaman hp

--- src/main/java/com/example/enemydata/toa/ampken/Shaman.java
+++ src/main/java/com/example/enemydata/toa/ampken/Shaman.java
@@ -7,7 +7,7 @@ import net.runelite.api.NPC;
 public class Shaman extends ToaEnemy {
     public Shaman(NPC npc, int invocation, int partySize, int pathLevel) {
         super(npc, invocation, partySize, pathLevel,
-                50, 60, 60, 20,
+                40, 60, 60, 20,
                 25, 0,
                 900, 900, 900, true);
     }
